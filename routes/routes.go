9342182b6d815541b04d3de1034745e47654ed6d
ilhamdika <ilham27dika@gmail.com>
2025-02-11 11:23:08 +0700

    feat: setup cors

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,28 @@ package routes
 
 import (
 	"backend_article/controllers"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{
+			"http://localhost:5173",
+			"http://localhost:3000",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	r.POST("/article", controllers.CreateArticle)
-	r.GET("/articles", controllers.GetArticles) 
 	r.GET("/articles/:limit/:offset", controllers.GetArticles)
 	r.GET("/article/:id", controllers.GetArticle)
 	r.PUT("/article/:id", controllers.UpdateArticle)
 	r.DELETE("/article/:id", controllers.DeleteArticle)
-	r.PUT("/article-status/:id", controllers.UpdateStatus)
 }
